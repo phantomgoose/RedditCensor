df9e8bcd0829714415332138a4cc3e316893eaa0
phantomgoose <alex.txt@gmail.com>
2017-12-24 23:44:39 -0800

    get rid of global variables

--- censor.js
+++ censor.js
@@ -1,26 +1,28 @@
-const searchStrings = ["trump"];
-const classesToCensor = ["title", "md"];
+(function() {
+  const searchStrings = ["trump"];
+  const classesToCensor = ["title", "md"];
 
-const processClass = className => {
-  const elements = document.getElementsByClassName(className);
-  Array.prototype.forEach.call(elements, element => {
-    processResult(element);
-  });
-};
-
-const processResult = element => {
-  searchStrings.forEach(string => {
-    if (element.innerText.toLowerCase().includes(string)) {
-      element.innerText = "-censored-";
-    }
-  });
-};
-
-const censorship = setInterval(() => {
-  if (document.readyState === "complete") {
-    classesToCensor.forEach(classTS => {
-      processClass(classTS);
+  const processClass = className => {
+    const elements = document.getElementsByClassName(className);
+    Array.prototype.forEach.call(elements, element => {
+      processResult(element);
     });
-    clearInterval(censorship);
-  }
-}, 333);
+  };
+
+  const processResult = element => {
+    searchStrings.forEach(string => {
+      if (element.innerText.toLowerCase().includes(string)) {
+        element.innerText = "-censored-";
+      }
+    });
+  };
+
+  const censorship = setInterval(() => {
+    if (document.readyState === "complete") {
+      classesToCensor.forEach(classTS => {
+        processClass(classTS);
+      });
+      clearInterval(censorship);
+    }
+  }, 333);
+})();
