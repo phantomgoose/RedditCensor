aa39d3a02c3243268af333a0b04013f3547f9acc
phantomgoose <alex.txt@gmail.com>
2017-12-24 23:31:43 -0800

    update dependecies

--- censor.js
+++ censor.js
@@ -1,21 +1,26 @@
-const processClass = (className) => {
+const searchStrings = ["trump"];
+const classesToCensor = ["title", "md"];
+
+const processClass = className => {
   const elements = document.getElementsByClassName(className);
-  Array.prototype.forEach.call(elements, element => { processResult(element); });
+  Array.prototype.forEach.call(elements, element => {
+    processResult(element);
+  });
 };
 
-const processResult = (element) => {
+const processResult = element => {
   searchStrings.forEach(string => {
     if (element.innerText.toLowerCase().includes(string)) {
-      element.innerText = '-censored-';
+      element.innerText = "-censored-";
     }
   });
 };
 
 const censorship = setInterval(() => {
-  if (document.readyState === 'complete') {
-    const searchStrings = ['diferent'];
-    const classesToCensor = ['title', 'md'];
-    classesToCensor.forEach(classTS => { processClass(classTS); });
+  if (document.readyState === "complete") {
+    classesToCensor.forEach(classTS => {
+      processClass(classTS);
+    });
     clearInterval(censorship);
   }
 }, 333);
